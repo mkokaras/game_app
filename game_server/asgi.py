45e4b7a203c3d7602eb5817c5652168b5299735a
mkokaras <menioskokaras1@gmail.com>
2022-01-17 18:19:22 +0200

    here

--- game_server/asgi.py
+++ game_server/asgi.py
@@ -6,18 +6,16 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
+from api.middleware import TokenAuthMiddleware
+import api.routing
 import os
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import api.routing
-from api.middleware import TokenAuthMiddleware
-from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_server.settings')
 
 asgi_application = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
     "http": asgi_application,
