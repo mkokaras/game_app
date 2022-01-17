c230f72e14d69573b03e2a0e7f24ab361f6e0138
Menelaos Kokaras <72735503+mkokaras@users.noreply.github.com>
2022-01-17 18:41:12 +0200

    Update asgi.py

--- game_server/asgi.py
+++ game_server/asgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_server.settings')
+
+django.setup()
 
 asgi_application = get_asgi_application()
 
