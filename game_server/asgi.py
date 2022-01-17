e5922f7cdf535590f27eaf5c558ef87d41f342ed
Menelaos Kokaras <72735503+mkokaras@users.noreply.github.com>
2022-01-17 18:08:17 +0200

    Update asgi.py

--- game_server/asgi.py
+++ game_server/asgi.py
@@ -11,13 +11,15 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import api.routing
-from api.middleware import TokenAuthMiddleware
-from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_server.settings')
 
 asgi_application = get_asgi_application()
+
+import api.routing
+from api.middleware import TokenAuthMiddleware
+from channels.routing import get_default_application
+
 
 application = ProtocolTypeRouter({
     "http": asgi_application,
